68aa992d84fe5a618c635c3686c7414f9be42817
ghdhddnr92 <ghdehddnr92@gmail.com>
2017-08-12 15:48:34 +0900

    라베에 올릴 DC motor test source code

---
+++ DC_motor_test.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+#include <signal.h>
+
+#define MOTOR 1 //GPIO 18
+#define DIRECTIOn 3 // GPIO 22
+
+void INThandler(int);
+void init();
+
+int main()
+{
+	printf("DC_motor TEST\n"):
+
+	wiringPiSetup();
+	signal(SIGINT, INThander);
+	
+	pinMode(MOTOR, OUTPUT);
+	pinMode(DIRECTION, OUTPUT);
+
+	pullUpDnControl(MOTOR, PUD_DOWN);
+	pullUpDnControl(DIRECTION, PUD_DOWN);
+
+	while(1)
+	{
+		digitalWrite(DIRECTION, HIGH);
+		digitalWrite(MOTOR, HIGH);
+		usleep(1000*500);
+		digitalWrite(Motor,LOW);
+		usleep(1000*500);
+		
+		digitalWrite(DIRECTION, LOW);
+		digitalWRite(MOTOR, HIGH);
+		usleep(1000*500);
+		digitalWrite(MOTOR, LOW);
+		usleep(1000*500);
+	}
+}
+void INThandler(int signal)
+{
+	printf("\ngoodbye\n");
+	init();
+	exit(0);
+}
+void init()
+{
+	digitalWrite(MOTOR, LOW);
+	digitalWrite(DIRECTION, LOW);
+}
